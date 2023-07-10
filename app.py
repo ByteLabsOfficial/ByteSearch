8b4a1ba60a84d878dd40ca346b56d98c9487ffd6
Iliya Soleimani <zalizadeh780@gmail.com>
2023-07-10 19:13:46 +0330

    Added Search Page

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+from duckduckgo_search import DDGS
+
+app = Flask(__name__)
+
+
+@app.route("/search")
+def get_search_results():
+    searchPrompt = request.args.get('q')
+
+    searchResults = []
+    with DDGS() as ddgs:
+        for r in ddgs.text(searchPrompt, region='wt-wt', safesearch='Off', timelimit='y'):
+            searchResults.append(r)
+    return render_template("index.html", searchPrompt=searchPrompt, searchResults=searchResults)
