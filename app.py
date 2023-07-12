addadb2de3cf19d53bcfcc4ad3565038bc44202f
Iliya Soleimani <zalizadeh780@gmail.com>
2023-07-12 13:28:21 +0330

    Started Adding Image Search

--- app.py
+++ app.py
@@ -13,4 +13,25 @@ def get_search_results():
     with DDGS() as ddgs:
         for r in ddgs.text(searchPrompt, region='wt-wt', safesearch='Off', timelimit='y'):
             searchResults.append(r)
-    return render_template("index.html", searchPrompt=searchPrompt, searchResults=searchResults)
+    return render_template("searchPage.html", searchPrompt=searchPrompt, searchResults=searchResults)
+
+
+@app.route("/image")
+def get_image_results():
+    searchPrompt = request.args.get('q')
+
+    searchResults = []
+    with DDGS() as ddgs:
+        keywords = searchPrompt
+        ddgs_images_gen = ddgs.images(
+            keywords,
+            region="wt-wt",
+            safesearch="Off",
+            size=None,
+            type_image=None,
+            layout=None,
+            license_image=None,
+        )
+        for r in ddgs_images_gen:
+            searchResults.append(r)
+    return render_template("imageSearch.html", searchPrompt=searchPrompt, searchResults=searchResults)
