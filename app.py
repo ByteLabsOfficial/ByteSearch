5e0e2b79aa764bd964637f81775149685231350f
Iliya Soleimani <zalizadeh780@gmail.com>
2023-09-20 23:15:53 +0330

    Fixed CORS

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
 from duckduckgo_search import DDGS
+from pytrends.request import TrendReq
 
 app = Flask(__name__)
 app.static_folder = "static"
+CORS(app, resources={r"/*": {"origins": "*"}})
+pytrends = TrendReq(hl='en-US', tz=360)
 
 
 @app.route("/")
@@ -24,6 +28,18 @@ def get_search_results():
     return render_template("searchPage.html", searchPrompt=searchPrompt, searchResults=searchResults, answerLst=answerLst)
 
 
+@app.route("/search_api", methods=["POST"])
+def get_search_results_api():
+    searchPrompt = request.args.get('q')
+
+    searchResults = []
+    with DDGS() as ddgs:
+        for r in ddgs.text(searchPrompt, region='wt-wt', safesearch='on', timelimit='y', backend="lite"):
+            searchResults.append(r)
+    print(searchResults)
+    return jsonify(searchResults)
+
+
 @app.route("/image")
 def get_image_results():
     searchPrompt = request.args.get('q')
@@ -43,3 +59,10 @@ def get_image_results():
         for r in ddgs_images_gen:
             searchResults.append(r)
     return render_template("imageSearch.html", searchPrompt=searchPrompt, searchResults=searchResults[0:100])
+
+
+@app.route('/trending', methods=["POST"])
+def get_trending_data():
+    trendsData = pytrends.trending_searches(pn='united_states')
+    trendsLst = trendsData.values.tolist()
+    return jsonify(trendsLst)
